ea5549ddaa3d51e733e360f0f7d6c22bb90fa345
Mark Cotton <tifosigoon@gmail.com>
2012-05-01 21:03:18 -0500

    Initial Commit

---
+++ Counter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Counter
+//
+//  Created by Mark Cotton on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
